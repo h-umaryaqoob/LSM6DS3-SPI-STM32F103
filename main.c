bd9fd96d8bef9c194cb2071f32273af1fb8b9f6e
H.M.Umar <37153940+Hafiz-Umar-Yaqoob@users.noreply.github.com>
2018-03-12 21:27:20 +0500

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  *  Created on: Sep 24, 2017
- *      Author: muaz waseem
+ *      Author: Umar Yaqoob
  */
 
 #include "main.h"
@@ -45,65 +45,3 @@ int main(void)
 	return 0;
 }
 
-void fatal_error_call_back(int8_t cause)
-{
-	for( ;; );
-}
-
-
-/*-----------------------------------------------------------*/
-
-void vApplicationMallocFailedHook( void )
-{
-	/* vApplicationMallocFailedHook() will only be called if
-	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-	function that will get called if a call to pvPortMalloc() fails.
-	pvPortMalloc() is called internally by the kernel whenever a task, queue,
-	timer or semaphore is created.  It is also called by various parts of the
-	demo application.  If heap_1.c or heap_2.c are used, then the size of the
-	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-	to query the size of free heap space that remains (although it does not
-	provide information on how the remaining heap might be fragmented). */
-	taskDISABLE_INTERRUPTS();
-	for( ;; );
-}
-/*-----------------------------------------------------------*/
-
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
-{
-	( void ) pcTaskName;
-	( void ) pxTask;
-
-	/* Run time stack overflow checking is performed if
-	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-	function is called if a stack overflow is detected. */
-	taskDISABLE_INTERRUPTS();
-	for( ;; );
-}
-
-void delay_x_ticks(uint32_t x)
-{
-	uint32_t start_tick = xTaskGetTickCount();
-	uint32_t tick_count;
-
-	do
-	{
-		if(xTaskGetTickCount() >= start_tick)
-		{
-			tick_count = xTaskGetTickCount() - start_tick;
-		}
-		else
-		{
-			tick_count = ((0xFFFFFFFFUL) - start_tick) + xTaskGetTickCount();
-		}
-
-	}while(tick_count < x);
-}
-
-void HardFault_Handler(void)
-{
-	//hard_power_off();
-	while(1)
-	{}
-}
